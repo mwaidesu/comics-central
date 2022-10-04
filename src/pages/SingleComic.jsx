6c02b453cfb24abd1fe66fb58cd904dcde1ce60c
Mwai <machariamwai@gmail.com>
2022-10-04 15:17:27 +0300

    Change format of Single Page comic

--- src/pages/SingleComic.jsx
+++ src/pages/SingleComic.jsx
@@ -3,10 +3,10 @@ import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import DownloadIcon from '@mui/icons-material/Download';
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import DownloadIcon from "@mui/icons-material/Download";
 
 function SingleComic() {
   return (
@@ -21,51 +21,65 @@ function SingleComic() {
             className="w-full h-[90vh] object-cover"
           />
         </div>
-        <div className="info-container flex-[1] py-0 px-[50px]">
+        <div className="info-container flex-[1] px-[50px] ">
           <h1 className="title text-[20px] font-bold">Justice League</h1>
-          <p className="description my-[20px]">
+          <p className="description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
             maiores ea sit officia provident ipsa nesciunt voluptas expedita ad
             possimus?
           </p>
-          <div>
-            <h1 className="title text-[20px] font-bold mt-2">Read Chapters Online</h1>
-            <hr />
 
-            <div>
-              <p className="my-[20px] flex items-center justify-between hover:font-extrabold">Chapter 1 <DownloadIcon className="hover:bg-black hover:text-white rounded-full"/></p>
-              <hr />
-            </div>
-
-            <div>
-              <p className="my-[20px] flex items-center justify-between hover:font-extrabold">Chapter 2<DownloadIcon className="hover:bg-black hover:text-white rounded-full"/></p>
-              <hr />
-            </div>
-            <div>
-              <p className="my-[20px] flex items-center justify-between hover:font-extrabold">Chapter 3 <DownloadIcon className="hover:bg-black hover:text-white rounded-full"/></p>
-              <hr />
-            </div>
-
-          </div>
-
-          <h1 className="font-bold text-[20px] mt-20">Buy Volume 1 Hardcopies Now!!</h1>
+          <h1 className="font-bold text-[20px] mt-[30px]">
+            Buy Volume 1 Hardcopies Now!!
+          </h1>
           <hr />
-
 
           <span className="price font-thin text-[40px]">Ksh. 750</span>
           <div className="add-container flex items-center justify-between mt-4">
             <div className="amount-container flex items-center font-bold">
               <button className="remove w-[30px] h-[30px] border border-red-500 bg-red-500 text-white flex items-center justify-center mx-[5px] hover:shadow-md hover:bg-red-600">
-                <RemoveIcon/>
+                <RemoveIcon />
               </button>
-              <span className="amount w-[30px] h-[30px] border border-red-500 flex items-center justify-center mx-[5px]">1</span>
+              <span className="amount w-[30px] h-[30px] border border-red-500 flex items-center justify-center mx-[5px]">
+                1
+              </span>
               <button className="add w-[30px] h-[30px] border border-red-500 bg-red-500 text-white flex items-center justify-center mx-[5px] hover:shadow-md hover:bg-red-600">
-                <AddIcon/>
+                <AddIcon />
               </button>
             </div>
-            <button className="p-[15px] border  hover:text-white cursor-pointer hover:shadow-md font-medium hover:bg-black">ADD TO CART &nbsp;
-            <AddShoppingCartIcon/>
+            <button className="p-[15px] border  hover:text-white cursor-pointer hover:shadow-md font-medium hover:bg-black">
+              ADD TO CART &nbsp;
+              <AddShoppingCartIcon />
             </button>
+          </div>
+          <div className="mt-[30px]">
+            <h1 className="title text-[20px] font-bold mt-2">
+              Read Chapters Online
+            </h1>
+            <hr />
+
+            <div>
+              <p className="my-[20px] flex items-center justify-between hover:font-extrabold">
+                Chapter 1{" "}
+                <DownloadIcon className="hover:bg-black hover:text-white rounded-full" />
+              </p>
+              <hr />
+            </div>
+
+            <div>
+              <p className="my-[20px] flex items-center justify-between hover:font-extrabold">
+                Chapter 2
+                <DownloadIcon className="hover:bg-black hover:text-white rounded-full" />
+              </p>
+              <hr />
+            </div>
+            <div>
+              <p className="my-[20px] flex items-center justify-between hover:font-extrabold">
+                Chapter 3{" "}
+                <DownloadIcon className="hover:bg-black hover:text-white rounded-full" />
+              </p>
+              <hr />
+            </div>
           </div>
         </div>
       </div>
